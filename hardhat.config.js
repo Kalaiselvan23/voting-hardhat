1e6cf68253f65645dc9d971104f31c038093e43d
Kalaiselvan <113838279+Kalaiselvan23@users.noreply.github.com>
2024-10-06 20:16:57 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ const SEPOLIA_PRIVATE_KEY=process.env.SEPOLIA_PRIVATE_KEY;
 const url=process.env.INFURA_URL;
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork:"hardhat",
   networks:{
     // sepolia:{
     //   url,
